8a5f020825603a4f3454f7ee958f6bf8f4d4c7c5
Alex Fedotov <alxfed@gmail.com>
2021-04-25 10:55:22 -0500

    initial commit

--- use.py
+++ use.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-"""...
+"""
+http://docs.cython.org/en/latest/src/quickstart/build.html#building-a-cython-module-using-setuptools
+https://www.jetbrains.com/help/pycharm/2021.1/cython.html#get-started-cython
 """
 from example import function
 
