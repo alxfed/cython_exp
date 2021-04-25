35084d0c334558eaea346631913f57d2d3a5b044
Alex Fedotov <alxfed@gmail.com>
2021-04-25 09:36:43 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cython_exp',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/alxfed/cython_exp',
+    license='MIT',
+    author='alxfed',
+    author_email='alxfed@gmail.com',
+    description='Cython experiments'
+)
